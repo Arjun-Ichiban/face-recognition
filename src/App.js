82facf762a792fe4df9bd71f79292ea54db9e2fb
Arjun S <83405143+Arjun-Ichiban@users.noreply.github.com>
2021-07-27 17:21:15 +0530

    Add FaceRecognition component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Particles from 'react-particles-js';
 
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import { Component } from 'react';
 
 import Clarifai from 'clarifai';
-import { func } from 'prop-types';
+// import { func } from 'prop-types';
 
 const app = new Clarifai.App({
   apiKey: '6443f1dfe1a24dd9b9a2326bb44272ee'
@@ -32,19 +33,20 @@ class App extends Component{
     super();
     this.state={
       input:'',
+      imageUrl:''
     }
   }
 
   onInputChange = (Event) =>{
-    console.log(Event.target.value);
+    this.setState({input: Event.target.value});
   }
 
   onButtonSubmit = () => {
-    console.log('click');
+    this.setState({imageUrl: this.state.input});
     app.models
     .predict(
       Clarifai.FACE_DETECT_MODEL,
-      "https://samples.clarifai.com/face-det.jpg")
+      this.state.input)
     .then(
       function(response){
         console.log(response);
@@ -69,8 +71,7 @@ class App extends Component{
           onButtonSubmit={this.onButtonSubmit}
         />
 
-        {/*
-        <FaceRecognition />*/}
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
 
       </div>
     );
